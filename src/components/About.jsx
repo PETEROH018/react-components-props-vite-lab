515f321f7d63e1e6a45c17ef0fd9da9160d292ee
PETEROH018 <mathengep63@gmail.com>
2026-04-30 16:31:07 +0300

    added a placeholder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 export default function About({blogData}){
-
+    const placeholder = "https://via.placeholder.com/215"
     return (
         <aside>
-          <img src={blogData.image} alt="Profile picture" />
+          <img src={blogData.image} alt="blog logo" onError={(e) => { e.target.src = placeholder; }} />
           <p>{blogData.about}</p>
         </aside>
     )
